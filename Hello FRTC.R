c73adaaa10d16ec86b73624d9c500fc6430469c6
TNS2029 <144594386+TNS2029@users.noreply.github.com>
2023-09-10 16:18:36 +0545

    Add files via upload

---
+++ Hello FRTC.R
@@ -0,0 +1,18 @@
+print("hello FRTC")
+a <- "FRTC"
+print(a)
+a <-2
+b <-3
+print("b")
+name<-"Tanka"
+name <-"Narayan"
+x <-2
+y <-3
+z <-"My name is Tanka Narayan Shrestha.I am 50 years Old"
+p <- x + y
+print(p)
+#variable x equals to 2
+x <-2
+x <-1:5
+y <-1:5
+plot(x,y)
